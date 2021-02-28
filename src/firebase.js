63277bee6a7bcb664df22f0718a47c6ea701b483
Shubham <shubhamnagpal30@gmail.com>
2021-02-28 16:22:32 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAx0IV081842tNjbhKZHNMyP14zKZJaiwY",
+  authDomain: "slack-clone-1bf30.firebaseapp.com",
+  projectId: "slack-clone-1bf30",
+  storageBucket: "slack-clone-1bf30.appspot.com",
+  messagingSenderId: "382314674744",
+  appId: "1:382314674744:web:254039c1e9f320df3644cd",
+  measurementId: "G-BW4M35625K",
+};
