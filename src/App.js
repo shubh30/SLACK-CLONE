340c5a559e3b28a77e95e1e02b59addac6ccd55c
Shubham <shubhamnagpal30@gmail.com>
2021-02-28 17:50:42 +0530

    Create Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Slack-Clone</h1>
+      <Router>
+        <>
+          <Switch>
+            <Route path="/">
+              <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
