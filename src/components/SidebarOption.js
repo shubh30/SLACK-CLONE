acc80bc8670528681c7bbe1369cd45356b3130d7
Shubham <shubhamnagpal30@gmail.com>
2021-03-15 17:35:51 +0530

    Use redux for Sidebar Options

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
+import { enterRoom } from "../features/appSlice";
 import { db } from "../firebase";
 
 function SidebarOption({ Icon, title, addChannelOption, id }) {
+  const dispatch = useDispatch();
   const addChannel = () => {
     const channelName = prompt("Please enter the channel name");
 
@@ -13,7 +16,15 @@ function SidebarOption({ Icon, title, addChannelOption, id }) {
     }
   };
 
-  const selectChannel = () => {};
+  const selectChannel = () => {
+    if (id) {
+      dispatch(
+        enterRoom({
+          roomId: id,
+        })
+      );
+    }
+  };
 
   return (
     <SidebarOptionContainer
